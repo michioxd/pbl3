67d3ab20c3cd796fac2f3b5b3708838ee218d475
huongkhthichhocit <nguyenthihuong221206@gmail.com>
2026-04-09 10:12:59 +0700

    feat(search): add fuzzy, accent-insensitive, multi-level search with ranking (top 15 results)

---
+++ server/Dtos/Province.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Pbl3.Dtos
+{
+    public class WardResponse
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; } = default!;
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = default!;
+    }
+
+    public class DistrictResponse
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; } = default!;
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = default!;
+
+        [JsonPropertyName("wards")]
+        public List<WardResponse> Wards { get; set; } = new List<WardResponse>();
+    }
+
+    public class ProvinceResponse
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; } = default!;
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = default!;
+
+        [JsonPropertyName("districts")]
+        public List<DistrictResponse> Districts { get; set; } = new List<DistrictResponse>();
+    }
+}
